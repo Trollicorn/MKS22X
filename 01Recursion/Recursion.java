4b0430297ec0893cb5675dee05320878fdb27f91
Mohammed Uddin <muddin3@stuy.edu>
2018-02-03 19:18:08 -0500

    did sqrt and tested

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -26,8 +26,44 @@ public class Recursion{
 	}
 
 	public double sqrt(double n){
-		
+		if (n < 0){
+			throw new IllegalArgumentException();
+		}
+		if (n == 0){
+			return 0;
+		}
+		return sqrtHelper(1,n);
 	}
 
+	private double sqrtHelper(double guess, double target){
+		if (Math.abs(target - Math.pow(guess,2)) < 0.0000000000001){
+			return guess;
+		}
+		guess = (target/guess + guess)/2;
+		return sqrtHelper(guess, target);
+	}
+
+	public static void main(String[] args){
+		Recursion test = new Recursion();
+
+		System.out.println(test.fact(0));
+		System.out.println(test.fact(1));
+		System.out.println(test.fact(10));
+
+		System.out.println(test.fib(0));
+		System.out.println(test.fib(1));
+		System.out.println(test.fib(2));		
+		System.out.println(test.fib(5));
+		System.out.println(test.fib(10)); 
+
+		System.out.println(test.sqrt(0));
+		System.out.println(test.sqrt(4));
+		System.out.println(test.sqrt(100));
+		System.out.println(test.sqrt(10));
+		System.out.println(test.sqrt(2));
+		System.out.println(test.sqrt(0.0001));
+		System.out.println(test.sqrt(0.001));
+
+	}
 
 }
