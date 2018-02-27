e103c926d6bc4d4c4d87c0ce84636b6c4a9b62c7
Trollicorn <muddin3@stuy.edu>
2018-02-27 14:46:14 -0500

    read file

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -2,38 +2,62 @@ import java.util.*;
 import java.io.*;
 public class Maze{
 
-    private char[][]maze;
+    private char[][] maze;
     private boolean animate;//false by default
 
     /*Constructor loads a maze text file, and sets animate to false by default.
 
-      1. The file contains a rectangular ascii maze, made with the following 4 characters:
-      '#' - Walls - locations that cannot be moved onto
-      ' ' - Empty Space - locations that can be moved onto
-      'E' - the location of the goal (exactly 1 per file)
-      'S' - the location of the start(exactly 1 per file)
+    1. The file contains a rectangular ascii maze, made with the following 4 characters:
+    '#' - Walls - locations that cannot be moved onto
+    ' ' - Empty Space - locations that can be moved onto
+    'E' - the location of the goal (exactly 1 per file)
+    'S' - the location of the start(exactly 1 per file)
+    2. The maze has a border of '#' around the edges. So you don't have to check for out of bounds!
 
-      2. The maze has a border of '#' around the edges. So you don't have to check for out of bounds!
-
-      3. When the file is not found OR the file is invalid (not exactly 1 E and 1 S) then: print a meaningful error and exit the program.
+    3. When the file is not found OR the file is invalid (not exactly 1 E and 1 S) then: print a meaningful error and exit the program.
     */
     public Maze(String filename){
-        //COMPLETE CONSTRUCTOR
+      //COMPLETE CONSTRUCTOR
+        try{
+            setMaze(filename);
+        }
+        catch (FileNotFoundException e){
+            throw new IllegalStateException();
+        }
+    }
+
+    private void setMaze(String filename) throws FileNotFoundException{
+
         File file = new File(filename);
         Scanner scan = new Scanner(file);
-        int rows = 0;
-        int cols = 0;
-        while ()
-        animate = false;
+        int rows,cols;                                         
+        rows = cols = 0;
+        while (scan.hasNextLine()){
+            String line = scan.nextLine();
+            if (cols == 0){
+                cols = line.length();
+            }
+            rows += 1;
+        }
+        boolean isGood;
+        maze = new char[rows][cols];
+        Scanner data = new Scanner(file);
+        for(int r = 0; r < rows; r++){
+            for (int c = 0; c < cols; c++){
+                
+            }
+        }
+
     }
-    
+
     private void wait(int millis){
-         try {
-             Thread.sleep(millis);
-         }
-         catch (InterruptedException e) {
-         }
-     }
+        try {
+            Thread.sleep(millis);
+        }
+        catch (InterruptedException e) {
+
+        }
+    }
 
     public void setAnimate(boolean b){
         animate = b;
@@ -46,8 +70,8 @@ public class Maze{
 
 
     /*Wrapper Solve Function returns the helper function
-      Note the helper function has the same name, but different parameters.
-      Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists.
+    Note the helper function has the same name, but different parameters.
+    Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists.
     */
     public int solve(){
             //find the location of the S. 
@@ -56,7 +80,7 @@ public class Maze{
 
             //and start solving at the location of the s.
             //return solve(???,???);
-      return -1;
+        return -1;
     }
 
     /*
@@ -84,6 +108,10 @@ public class Maze{
 
         //COMPLETE SOLVE
         return -1; //so it compiles
+    }
+
+    public static void main(String[] args){
+        Maze j = new Maze("data1.dat");
     }
 
 }
