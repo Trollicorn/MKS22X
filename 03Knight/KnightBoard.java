ff0512fb13dca54b9c5125be0fc6f4273baffeb0
Mohammed Uddin <muddin3@stuy.edu>
2018-03-05 20:29:41 -0500

    reworked code format of countSolutions from 02NQueens and fit it into countSolutions here (and it works)

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -2,7 +2,7 @@ public class KnightBoard{
 
 	private int[][] board;                                                                      // _ 7 _ 0 _        
 	private int[][] moves = {{-2,1},{-1,2},{1,2},{2,1},{2,-1},{1,-2},{-1,-2},{-2,-1}};          // 6 _ _ _ 1   
-										                                                        // _ _ K _ _ 
+	private int solutions;				                                                        // _ _ K _ _ 
 																				          		// 5 _ _ _ 2
 																				                // _ 4 _ 3 _
 	public KnightBoard(int startingRows, int startingCols){
@@ -60,8 +60,30 @@ public class KnightBoard{
 
 
 
-	public int countSolutions(){
-		return 1;
+	public int countSolutions(int row, int col){
+		if (isBad()){
+			throw new IllegalStateException();
+		}
+		solutions = 0;
+		countHelp(row, col, 1);
+		return solutions;
+	}
+
+	private void countHelp(int row, int col, int level){
+		board[row][col] = level; 
+		if (level == board.length * board[0].length){
+			solutions += 1;
+		}
+		for (int mode = 0; mode < 8; mode++){
+			int r = row + moves[mode][0];
+			int c = col + moves[mode][1];
+
+			if (r > -1 && r < board.length && c > -1 && c < board.length && board[r][c] == 0){
+				countHelp(r,c,level+1);
+				board[r][c] = 0;
+			}
+		}
+		return;
 	}
 
 	private boolean isBad(){
@@ -77,9 +99,10 @@ public class KnightBoard{
 
 
 	public static void main(String[] args){
-		KnightBoard b = new KnightBoard(8, 8);
+		KnightBoard b = new KnightBoard(5, 5);
 		System.out.println(b);
-		System.out.println(b.solve(0,0));
+	//	System.out.println(b.solve(0,0));
+		System.out.println(b.countSolutions(0,0));
 		System.out.println(b);
 	}
 
