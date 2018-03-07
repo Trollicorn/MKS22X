5fffa1175dc5b48efd4416f73652cc070ce39cc0
Trollicorn <muddin3@stuy.edu>
2018-03-07 14:45:44 -0500

    worked on bronze

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -1,6 +1,30 @@
+import java.util.*;
+import java.io.*;
+
 public class USACO{
 
 	public static int bronze(String filename){
+		//USACO Bronze Mar08 Problem 12
+
+		int[][] ok;
+		
+		try{
+			int[] info = new int[4];
+			File file = new File(filename);
+			Scanner scan = new Scanner(file);
+			String line = scan.nextLine();
+			String num = "";
+			for (int i = 0; i < line.length(); i++){
+				if (line.charAt(i) == ' '){
+					
+				}
+				num += line.charAt(i);
+			}
+		}
+		catch (FileNotFoundException e){
+			System.out.println("File Not Found");
+			System.exit(1);
+		}
 		return -1;
 	}
 
