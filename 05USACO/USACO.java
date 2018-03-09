17d61bbdc4517cafd88b9e200baa783ba0b2d8ab
Trollicorn <muddin3@stuy.edu>
2018-03-09 14:45:14 -0500

    finished bronze

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -17,7 +17,30 @@ public class USACO{
 			col = Integer.parseInt(scan.next());
 			depth = Integer.parseInt(scan.next());
 			instructions = Integer.parseInt(scan.next());
-			System.out.println(scan.nextLine());
+			//scan.nextLine();
+			ok = new int[row][col];
+			for (int r = 0; r < row; r++){
+				for (int c = 0; c < col; c++){
+					ok[r][c] = Integer.parseInt(scan.next());
+				}
+			}
+			for (int i = 0; i < instructions; i++){
+				int r = Integer.parseInt(scan.next()) - 1;
+				int	c = Integer.parseInt(scan.next()) - 1;
+				int amount = Integer.parseInt(scan.next());
+				lower(r, c, amount, ok);
+			}
+			int sum = 0;
+			for (int r = 0; r < row; r++){
+				for (int c = 0; c < col; c++){
+					if (depth - ok[r][c] > 0){
+						sum += depth - ok[r][c];
+					}
+				}
+			}
+			return sum * 72 * 72;
+
+			//System.out.println(scan.nextLine());
 		}
 		catch (FileNotFoundException e){
 			System.out.println("File Not Found");
@@ -26,13 +49,31 @@ public class USACO{
 		return -1;
 	}
 
+	private static void lower(int r, int c, int amount, int [][] ok){
+		int highest = 0;
+		for (int i = r; i < r + 3; i++){
+			for (int j = c; j < c + 3; j++){
+				if (ok[i][j] > highest){
+					highest = ok[i][j];
+				}
+			}
+		}
+		for (int i = r; i < r + 3; i++){
+			for (int j = c; j < c + 3; j++){
+				if (ok[i][j] > highest - amount){
+					ok[i][j] = highest - amount;
+				}
+			}
+		}
+	}
+
 	public static int silver(String filename){
 
 		return -1;
 	}
 
 	public static void main(String[] args){
-		USACO.bronze("makelake.in");
+		System.out.println(USACO.bronze("makelake.in"));
 	}
 
 }
