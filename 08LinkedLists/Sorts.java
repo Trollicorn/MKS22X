136f9a0be65f5986545ad80e90fd556261087d21
Trollicorn <muddin3@stuy.edu>
2018-04-17 14:50:55 -0400

    added new labs and worked on radixsort with negatives

--- 08LinkedLists/Sorts.java
+++ 08LinkedLists/Sorts.java
@@ -40,6 +40,9 @@ public class Sorts{
 		}
 		data.clear();
 
+		System.out.println(pos);
+		System.out.println(neg);
+
 		int max = pos.get(pos.max());
 		int digitP = (int)Math.log10(max) + 1;
 		for (int d = 0; d < digitP; d++){
@@ -47,7 +50,7 @@ public class Sorts{
 				int num = (j / (int)Math.pow(10,d)) % 10;
 				bucket[num].add(j);
 			}
-			data.clear();
+			pos.clear();
 			for (int i = 0; i < 10; i++){
 				pos.extend(bucket[i]);
 			}
@@ -63,43 +66,18 @@ public class Sorts{
 				int num = (-1 * j / (int) Math.pow(10,d)) % 10;
 				bucket[9 - num].add(j);
 			}
-			data.clear();
+			neg.clear();
 			for (int i = 0; i < 10; i++){
-				data.extend(bucket[i]);
+				neg.extend(bucket[i]);
 			}
 		}
+
+	//	System.out.println(neg);
+	//	System.out.println(pos);
 
 		data.extend(neg);
 		data.extend(pos);
 
-
-
-/*		@SuppressWarnings("unchecked") MyLinkedListImproved<Integer>[] bucket = new MyLinkedListImproved[10];
-		MyLinkedListImproved<Integer> pos = new MyLinkedListImproved<>();
-		int i = 0;
-		for (Integer j : data){
-			if (j >= 0){
-				pos.add(data.remove(i));
-			}else{
-				i++;
-			}
-		}
-		int max = pos.get(pos.max());
-		int min = data.get(data.min());
-		int digitP = (int) Math.ceil(Math.log10(max));
-		int digitN= (int) Math.ceil(Math.log10(min * -1));
-		
-		for	(int dig = 1; dig < digitN + 1; dig++){
-			for (Integer j : data){
-				int temp = (j / (int)Math.pow(10,dig - 1)) % 10;
-				bucket[temp].add(j);
-				data.remove(0);
-			}
-			for (int k = 0; k < 10; k++){
-				data.extend(bucket[9-k]);
-			}
-		}
-*/
 	}
 
 	public static void main(String[] args){
