d16d0bc789f0a69e67a04d5c4baae769ed9a8710
Mohammed Uddin <muddin3@stuy.edu>
2018-04-16 23:30:50 -0400

    worked on making radixsort work with negatives, but failed

--- 08LinkedLists/Sorts.java
+++ 08LinkedLists/Sorts.java
@@ -23,6 +23,51 @@ public class Sorts{
 
 	public static void radixsortIncludingNegatives(MyLinkedListImproved<Integer> data){
 
+		@SuppressWarnings("unchecked") MyLinkedListImproved<Integer>[] bucket = new MyLinkedListImproved[10];
+		MyLinkedListImproved<Integer> pos = new MyLinkedListImproved<>();
+		for (int m = 0; m < 10; m++){
+			bucket[m] = new MyLinkedListImproved<Integer>();
+		}
+
+		int k = 0;
+		for (Integer j : data){
+			if (j >= 0){
+				pos.add(data.remove(k));
+			}else{
+				k++;
+			}
+		}
+
+		int max = pos.get(pos.max());
+		int digitP = (int)Math.log10(max) + 1;
+		for (int d = 0; d < digitP; d++){
+			for (Integer j : pos){
+				int num = (j / (int)Math.pow(10,d)) % 10;
+				bucket[num].add(j);
+			}
+			data.clear();
+			for (int i = 0; i < 10; i++){
+				pos.extend(bucket[i]);
+			}
+		}
+
+		int min = data.get(data.min());
+		int digitN = (int)Math.log10(-1 * min) + 1;
+		for (int d = 0; d < digitN; d++){
+			for (Integer j : data){
+				int num = (-1 * j / (int) Math.pow(10,d)) % 10;
+				bucket[9 - num].add(j);
+			}
+			data.clear();
+			for (int i = 0; i < 10; i++){
+				data.extend(bucket[i]);
+			}
+		}
+
+		data.extend(pos);
+
+
+
 /*		@SuppressWarnings("unchecked") MyLinkedListImproved<Integer>[] bucket = new MyLinkedListImproved[10];
 		MyLinkedListImproved<Integer> pos = new MyLinkedListImproved<>();
 		int i = 0;
@@ -54,10 +99,10 @@ public class Sorts{
 	public static void main(String[] args){
 		MyLinkedListImproved<Integer> L = new MyLinkedListImproved<>();
 		for (int i = 0; i < 6; i++){
-			L.add( (int)(Math.random() * 20));
+			L.add( (int)(Math.random() * 20) - 10);
 		}
 		System.out.println(L);
-		radixsort(L);
+		radixsortIncludingNegatives(L);
 		System.out.println(L);
 	}
 
