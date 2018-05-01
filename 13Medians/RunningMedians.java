537f71fdbca573d281d45f01e6774a666a9f6bd9
Trollicorn <muddin3@stuy.edu>
2018-05-01 14:50:38 -0400

    save

--- 13Medians/RunningMedians.java
+++ 13Medians/RunningMedians.java
@@ -1,3 +1,9 @@
 public class RunningMedians{
 	
+	private MyHeap<Integer> maxHeap, minHeap;
+
+	public RunningMedians(){
+		maxHeap = new MyHeap<Integer>();
+		minHeap = new MyHeap<Integer>(false);
+	}
 }
