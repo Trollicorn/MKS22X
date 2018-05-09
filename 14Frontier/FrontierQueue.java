2dc96c02a308e8f85cf7067e63a018aff5a08f78
Mohammed Uddin <muddin3@stuy.edu>
2018-05-08 23:42:58 -0400

    wrote Frontier classes

--- 14Frontier/FrontierQueue.java
+++ 14Frontier/FrontierQueue.java
@@ -2,4 +2,19 @@ import java.util.*;
 
 public class FrontierQueue implements Frontier{
 
+	private LinkedList<Location> L = new LinkedList<>();
+
+	public Location next(){
+		return L.remove();
+	}
+
+	public void add(Location n){
+		L.add(n);
+	}
+
+	public boolean hasNext(){
+		return L.size() != 0;
+	}
+
+
 }
--- 14Frontier/FrontierStack.java
+++ 14Frontier/FrontierStack.java
@@ -2,4 +2,19 @@ import java.util.*;
 
 public class FrontierStack implements Frontier{
 
+	private Stack<Location> L = new Stack<>();
+
+	public Location next(){
+		return L.pop();
+	}
+	
+	public void add(Location n){
+		L.push(n);
+	}
+	
+	public boolean hasNext(){
+		return L.size() != 0;
+	}
+	}
+
 }
