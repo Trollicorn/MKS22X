3c0f98accef967a0588ceb2ae5ecaeac7ff23ecf
Mohammed Uddin <muddin3@stuy.edu>
2018-06-03 23:29:23 -0400

    added test files to Frontier, attempted fixing heapsort

--- 12MyHeap/Sorts.java
+++ 12MyHeap/Sorts.java
@@ -2,14 +2,29 @@ import java.util.Arrays;
 
 public class Sorts{
 
+	public static void heapsortNew(int[] data){
+		for (int i = 0; i < data.length; i++){
+			heapify(data, i);
+		}
+	}
+
 	public static void heapsort(int[] data){
+		MyHeap<Integer> heap = new MyHeap<>();
+		for (int i = 0; i < data.length; i++){
+			heap.add(data[i]);
+		}
+		while (heap.size() != 0){
+			data[heap.size() - 1] = heap.remove();
+		}
+	}
+
+	private static void heapify(int[] data, int begin){
 		int i = data.length - 1;
-		while (i > -1){
+		while (i > begin ){
 			if (!pushUp(data, i)){
 				i--;
 			}
 		}
-
 	}
 
 	private static boolean pushUp(int[] data, int n){
@@ -37,7 +52,7 @@ public class Sorts{
 	public static void main(String[] args){
 		int[] b = {4, 2, 3, 5, 6, 1, 4, 6, 1, 5, 7,3, 9, 0, 3};
 		System.out.println(Arrays.toString(b));
-		heapsort(b);
+		heapsortNew(b);
 		System.out.println(Arrays.toString(b));
 	}
 
